171fcf10dda3c7ed65b7d2f9b819d100ec7e8116
fattone225 <andyusnik@gmail.com>
2023-07-10 00:55:54 -0600

    Set phone input

--- input-component/src/App.jsx
+++ input-component/src/App.jsx
@@ -1,13 +1,16 @@
 
 import './App.css'
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { TextInput } from './TextInput';
+import { PhoneInput } from './PhoneInput';
 
 
 function App() {
   const [nameInput, setNameInput] = useState("");
   const [ageInput, setAgeInput] = useState("");
   const [sloganInput, setSloganInput] = useState("");
+
+  const sloganRef = useRef(null)
 
   const reset = () => {
     setNameInput("")
@@ -26,7 +29,10 @@ function App() {
             sloganInput
           })
           reset()
-      }}>
+        }}>
+        <button onClick={() => {
+          sloganRef.current?.scrollIntoView();
+        }}>  Scroll to Slogan</button>
         <h3>Information form</h3>
         <TextInput
           //prop spreading, setting up inputProps as an object with key value pairs, placeholder is not a prop, but
@@ -50,16 +56,76 @@ function App() {
           }}
           labelText={"Age"}
         />
+        {"a"
+          .repeat(50)
+          .split("")
+          .map((_, index) => (
+          <TextInput
+            key={index}
+            inputProps={{
+              placeholder: "HHERRRO",
+              ref:sloganRef,
+            }}
+            labelText={"herro"}
+          />
+        ))}
         <TextInput
           inputProps={{
             onChange: (e) => {
               setSloganInput(e.target.value);
               },
             value: sloganInput, 
-            placeholder: "I know nothing"
+            placeholder: "I know nothing",
+            id: "my-input",
+            style: {
+              backgroundColor: "tomato"
+            }
           }}
           labelText={"Slogan"}
         />
+
+{"a"
+          .repeat(50)
+          .split("")
+          .map((_, index) => (
+          <TextInput
+            key={index}
+            inputProps={{
+              placeholder: "HHERRRO",
+              ref:sloganRef,
+            }}
+            labelText={"herro"}
+          />
+          ))}
+        
+        {"a"
+          .repeat(50)
+          .split("")
+          .map((_, index) => (
+          <TextInput
+            key={index}
+            inputProps={{
+              placeholder: "HHERRRO",
+              ref:sloganRef,
+            }}
+            labelText={"herro"}
+          />
+          ))}
+        
+        {"a"
+          .repeat(50)
+          .split("")
+          .map((_, index) => (
+          <TextInput
+            key={index}
+            inputProps={{
+              placeholder: "HHERRRO",
+              ref:sloganRef,
+            }}
+            labelText={"herro"}
+          />
+        ))}
+
         {/* <div>
           <label htmlFor="age">Age:</label>
             <input
@@ -82,6 +148,7 @@ function App() {
               value={sloganInput}
             />
         </div> */}
+        <PhoneInput />
         <input type="submit" value="Submit" />
       </form>
 
